59b7d4b62eaf84b15e95a510cfceac3fe35b8b5c
unknown <veena@gho.care>
2026-01-27 18:12:23 +0530

    book-appointment

--- src/app/schedule/doctor-details/doctor-details.ts
+++ src/app/schedule/doctor-details/doctor-details.ts
@@ -1,13 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'doctor-details',
-  imports: [CommonModule],
+  imports: [CommonModule,MatIconModule,MatCardModule],
   templateUrl: './doctor-details.html',
 })
 export class DoctorDetails implements OnInit {
-  @Input() doctor: [];
+  @Input() docinfo: any;
 
   ngOnInit(): void {
     
