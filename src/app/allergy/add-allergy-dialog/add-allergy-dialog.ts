cac29b5cef8bf0bca16b3e2915f0dfebd21993c5
unknown <veena@gho.care>
2026-01-29 13:10:28 +0530

    allergy-update

--- src/app/allergy/add-allergy-dialog/add-allergy-dialog.ts
+++ src/app/allergy/add-allergy-dialog/add-allergy-dialog.ts
@@ -1,29 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { GHOService } from '../../services/ghosrvs';
 import { ghoresult, tags } from '../../model/ghomodel';
 import { catchError } from 'rxjs';
 
-
 @Component({
   selector: 'add-allergy-dialog',
-  imports: [MatButtonModule,
+  imports: [
+    MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatRadioModule,
     MatDialogModule,
     FormsModule,
-    CommonModule,],
+    CommonModule
+  ],
   templateUrl: './add-allergy-dialog.html',
-  styleUrl: './add-allergy-dialog.css',
+  styleUrls: ['./add-allergy-dialog.css'],
 })
 export class AddAllergyDialog {
 
@@ -35,33 +37,51 @@ export class AddAllergyDialog {
   reaction = '';
   diagnosedBy = '';
   severity = '';
+  allergyId: any = null; 
 
   dialogRef = inject(MatDialogRef<AddAllergyDialog>);
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    if (data) {
+      this.allergyId = data.ID;
+      this.allergyName = data.AllergyName;
+      this.reaction = data.Reaction;
+      this.diagnosedBy = data.DiagnosedBy || '';
+      this.severity = data.Severity || '';
+    }
+  }
 
   save() {
     this.tv = [
       { T: "dk1", V: this.srv.getsession('id') },
+      { T: "dk2", V: this.allergyId },
       { T: "c1", V: this.allergyName },
       { T: "c2", V: this.reaction },
       { T: "c3", V: this.diagnosedBy },
       { T: "c4", V: this.severity },
-      { T: "c10", V: "1" }
+      { T: "c10", V: this.allergyId ? "2" : "1" }
     ];
+
+    if (this.allergyId) {
+      this.tv.push({ T: "dk2", V: this.allergyId });
+    }
 
     this.srv.getdata("PatientAllergy", this.tv).pipe(
       catchError((err) => {
-        this.srv.openDialog("Allergy Info", "e", "Error while adding allergy");
+        this.srv.openDialog("Allergy Info", "e", this.allergyId ? "Error while updating allergy" : "Error while adding allergy");
         throw err;
       })
     ).subscribe((r) => {
       if (r.Status === 1) {
-        this.srv.openDialog("Success", "s", "Allergy added successfully!");
+        this.srv.openDialog("Success", "s", this.allergyId ? "Allergy updated successfully!" : "Allergy added successfully!");
         this.dialogRef.close(true);
-
       } else {
-        this.srv.openDialog("Error", "e", "Failed to add allergy");
+        this.srv.openDialog("Error", "e", this.allergyId ? "Failed to update allergy" : "Failed to add allergy");
       }
     });
   }
-}
 
+  cancel() {
+    this.dialogRef.close();
+  }
+}
--- src/app/allergy/allergy.ts
+++ src/app/allergy/allergy.ts
@@ -47,6 +47,19 @@ export class Allergy implements OnInit {
 
   }
 
+  editAllergy(allergy: any) {
+    console.log(allergy)
+    const dialogRef = this.dialog.open(AddAllergyDialog, {
+      width: '600px',
+      disableClose: false,
+      data: allergy
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.getAllergies();
+    });
+  }
+  
   deleteAllergy(id: any, name: string) {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
       width: '400px',
@@ -84,9 +97,14 @@ export class Allergy implements OnInit {
   }
 
   addAllergy() {
-    this.dialog.open(AddAllergyDialog, {
+    const dialogRef = this.dialog.open(AddAllergyDialog, {
       width: '600px',
       disableClose: false,
     });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.getAllergies();
+    });
   }
+
 }
