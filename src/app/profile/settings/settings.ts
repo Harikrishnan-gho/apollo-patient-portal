946b59c70c8c12c049ff09bb621a2cf7c8575895
Nandana-GHO <nandana@gho.care>
2026-01-31 12:25:22 +0530

    added profile page and settings

--- src/app/profile/settings/settings.ts
+++ src/app/profile/settings/settings.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -8,5 +9,8 @@ import { MatIcon } from '@angular/material/icon';
   styleUrl: './settings.css',
 })
 export class Settings {
-
+  router=inject(Router)
+account(){
+  this.router.navigate(['profile'])
+}
 }
