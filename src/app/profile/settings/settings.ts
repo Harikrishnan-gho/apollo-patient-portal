6a0cea989f35dda080bf5d021beeb6efc6061304
Nandana-GHO <nandana@gho.care>
2026-01-31 13:16:04 +0530

    designed the settings page

--- src/app/profile/settings/settings.ts
+++ src/app/profile/settings/settings.ts
@@ -1,15 +1,23 @@
 import { Component, inject } from '@angular/core';
+import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
-  imports: [MatIcon],
+  imports: [MatIcon,MatDivider,MatSlideToggleModule],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
 export class Settings {
+
   router=inject(Router)
+   defaultImage =
+    'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg';
+
+  previewImage: string | ArrayBuffer | null = null;
+
 account(){
   this.router.navigate(['profile'])
 }
